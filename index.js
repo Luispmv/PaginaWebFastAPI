f3d1617cb980d15a48b05249f0ca89ec4340af9c
Luispmv <Luispmv>
2024-06-11 00:49:09 -0600

    Consumiendo nuestra API, lo que hacemos aqui es hacer un fetch a la url de categorias y por cada categoria existente se nos imprimira esta

--- index.js
+++ index.js
@@ -6,17 +6,34 @@ botontoggle = document.getElementById("toggleBtn")
 })
 
 
-function obteniendoCategorias(done){
-    const results = fetch("http://137.184.185.43/categorias")
+function obteniendoCategorias(done) {
+    const results = fetch("http://137.184.185.43/categorias");
     results
         .then(response => response.json())
         .then(data => {
-            done(data)
-        })
+            console.log(typeof(data))
+            done(data);
+        });
 }
 
-obteniendoCategorias(data =>{
-    console.log(data)
-})
+obteniendoCategorias(data => {
+    data.forEach(categoria => {
+        const card_categories = document.createRange().createContextualFragment(
+            `
+            <button class="card-categories">
+                <img src="${categoria.image}" alt="">
+                <p>${categoria.name}</p>
+            </button>
+            `
+        );
+
+        const categorias = document.querySelector(".categorias-grid"); // Cambiado de "categorias-grid" a ".categorias-grid"
+        categorias.append(card_categories);
+    });
+});
+
+
+
+
 
 
