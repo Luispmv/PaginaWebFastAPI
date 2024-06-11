2f05e0227d95d5c9cd39e3ad5d93e4cf8b641a98
Luispmv <Luispmv>
2024-06-10 23:17:49 -0600

    Prueba 3

--- index.js
+++ index.js
@@ -6,35 +6,35 @@ botontoggle = document.getElementById("toggleBtn")
 })
 
 
-function obteniendoCategorias(done){
-    const results = fetch("http://137.184.185.43/categorias")
-    results
-        .then(response => response.json())
-        .then(data => {
-            done(data)
-        })
-}
-
-obteniendoCategorias(data =>{
-    console.log(data)
-})
-
 // function obteniendoCategorias(done){
-//     fetch("http://137.184.185.43/categorias", { mode: 'no-cors' })
-//         .then(response => {
-//             // La respuesta estará limitada en este modo
-//             if (!response.ok) {
-//                 throw new Error(`Network response was not ok: ${response.statusText}`);
-//             }
-//             return response.json();
-//         })
+//     const results = fetch("http://137.184.185.43/categorias")
+//     results
+//         .then(response => response.json())
 //         .then(data => {
-//             done(data);
+//             done(data)
 //         })
-//         .catch(error => {
-//             console.error('There has been a problem with your fetch operation:', error);
-//         });
 // }
+
+// obteniendoCategorias(data =>{
+//     console.log(data)
+// })
+
+function obteniendoCategorias(done){
+    fetch("http://137.184.185.43/categorias", { mode: 'no-cors' })
+        .then(response => {
+            // La respuesta estará limitada en este modo
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            done(data);
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
 
 obteniendoCategorias(data => {
     console.log(data);
